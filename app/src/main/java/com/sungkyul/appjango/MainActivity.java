452e23b09847d1c5f2a7754f46af02fdfed3d4e9
jenny <12venus08@naver.com>
2020-10-28 14:34:55 +0900

    main화면 setting intent

--- app/src/main/java/com/sungkyul/appjango/MainActivity.java
+++ app/src/main/java/com/sungkyul/appjango/MainActivity.java
@@ -2,7 +2,12 @@ package com.sungkyul.appjango;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("ACTIVITY_LC", "onCreate 호출됨");
+        Toast.makeText(getApplicationContext(),"onCreate 호출됨",Toast.LENGTH_SHORT).show();
+
+        Button button = (Button) findViewById(R.id.setting);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
+                intent.putExtra("message", "반갑습니다.");
+                startActivity(intent);
+            }
+        });
     }
+
 }
